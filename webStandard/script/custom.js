d3555e2e7ae6d9a9f5d3d734d5d20fd1fa79d201
Oh Ga eun <dhrkdms4113@naver.com>
2022-07-01 15:59:44 +0900

    웹표준사이트 완성

--- webStandard/script/custom.js
+++ webStandard/script/custom.js
@@ -15,14 +15,76 @@ $(".ban").slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
     dots: true
 });
 
+// tab_menu
+var $tab_list = $(".tab_menu");
 
+$tab_list.find("ul ul").hide();
+$tab_list.find("li.active > ul").show();
 
+function tabMenu(e){
+    e.preventDefault();
+    var $this = $(this);
+    $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
+}
+$tab_list.find("ul > li > a").click(tabMenu).focus(tabMenu);
 
+// 갤러리
+$(".gallery_img").slick({
+    fade: true,
+    pauseOnHover: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 1
+});
 
+$(".play").click(function(){
+    $(".gallery_img").slick("slickPlay");
+});
+$(".pause").click(function(){
+    $(".gallery_img").slick("slickPause");
+});
+$(".prev").click(function(){
+    $(".gallery_img").slick("slickPrev");
+});
+$(".next").click(function(){
+    $(".gallery_img").slick("slickNext");
+});
+
+// 윈도우 팝업
+$(".window").click(function(e){
+    e.preventDefault();
+    // window.open("파일명","팝업이름","옵션설정")
+    // 옵션 : left, top, width, height, status, location, scroollbars ...
+    window.open("popup.html","popup01","width=800, height=590, left=50, top=50, scrollbars=0, toolbar=0, menubar=0");
+});
+
+// 레이어 팝업
+$(".layer").click(function(e){
+    e.preventDefault();
+    // $("#layer").css("display","block");
+    // $("#layer").show();
+    // $("#layer").fadeIn();
+    $("#layer").slideDown();
+});
+$("#layer .close").click(function(e){
+    e.preventDefault();
+    $("#layer").slideUp();
+});
+
+// 라이트 박스
+$(".lightgallery").lightGallery({
+    thembnail: true,
+    autoplay: true,
+    pause: 3000,
+    progressBar: true
+}); 
 
 
 
