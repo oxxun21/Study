5b214d6a617dc12e0ba0704b5728a04999d93764
Oh Ga eun <dhrkdms4113@naver.com>
2022-07-05 21:35:17 +0900

    영화 차트

--- Megabox/assets/script/custom.js
+++ Megabox/assets/script/custom.js
@@ -1,3 +1,4 @@
+// 배너 이미지 슬라이드
 var swiper = new Swiper('.swiper-container',{
     pagination: {
         el: '.swiper-pagination',
@@ -9,4 +10,20 @@ var swiper = new Swiper('.swiper-container',{
     autoplay: {
         delay: 5000,
     },
+});
+
+// 영화차트 탭 메뉴
+var movBtn = $(".movie_title > ul > li");
+var movCont = $(".movie_chart > div");
+
+movCont.hide().eq(0).show();
+
+movBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    movBtn.removeClass("active");
+    target.addClass("active");
+    movCont.css("display","none");
+    movCont.eq(index).css("display","block");
 });
