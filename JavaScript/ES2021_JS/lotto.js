c2162b2e6ef2aeb499d67c40beab9706a4f83a2a
Oh Ga eun <dhrkdms4113@naver.com>
2022-08-31 15:17:49 +0900

    로또 구현 완성

--- JavaScript/ES2021_JS/lotto.js
+++ JavaScript/ES2021_JS/lotto.js
@@ -1,3 +1,4 @@
+// 피셔 에이츠 셔플 알고리즘
 const candiate = Array(45).fill().map((v, i) => i + 1);
 
 const shuffle = [];
@@ -28,14 +29,44 @@ console.log(winBalls, bonus);
 const $result = document.querySelector('#result');
 const $bonus = document.querySelector('#bonus');
 
+function colorize(number, $tag) {
+    if (number < 10) {
+        $tag.style.backgroundColor = 'red';
+        $tag.style.color = 'white';
+    } else if (number < 20) {
+        $tag.style.backgroundColor = 'orange';
+    } else if (number < 30) {
+        $tag.style.backgroundColor = 'yellow';
+    } else if (number < 40) {
+        $tag.style.backgroundColor = 'blue';
+        $tag.style.color = 'white';
+    } else {
+        $tag.style.backgroundColor = 'green';
+        $tag.style.color = 'white';
+    }
+}
+
 const showBall = (number, $target) => {
     const $ball = document.createElement('div');
     $ball.className = 'ball';
+    colorize(number, $ball);
     $ball.textContent = number;
     $target.appendChild($ball);
 }
 
-for (let i = 0; i < 6; i++){
+// var 함수 레벨 스코프 > 클로저(함수와 함수 바깥 변수의 관계) 사용해서 오류 해결
+// for (var i = 0; i < winBalls.length; i++){
+//     (function(j){
+//         setTimeout(() => {
+//             showBall(winBalls[j], $result);
+//         }, (i + 1) * 1000);
+//     })(i);  // 즉시 실행 함수로 호출
+// }
+// 함수 스코프 var와 비동기 함수가 만나면 클로저 문제 발생
+
+
+// let 블록 레벨 스코프
+for (let i = 0; i < winBalls.length; i++){
     setTimeout(() => {
         showBall(winBalls[i], $result);
     }, (i + 1) * 1000);
