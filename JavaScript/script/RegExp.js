b552f1e17af254e1a69de816b6a107be2ed87b68
Oh Ga eun <dhrkdms4113@naver.com>
2022-09-05 20:23:27 +0900

    파일 정리

--- JavaScript/script/RegExp.js
+++
@@ -1,9 +0,0 @@
-var pattern = /(\w+)\s(\w+)/;
-
-var str = "coding everybody";
-
-var result = str.replace(pattern,"$2, $1");
-// $2 : 2번째 그룹, $1 : 1번째 그룹
-// 두번째 그룹 + , +/s(띄어쓰기) + 첫번째 그룹
-
-console.log(result);
